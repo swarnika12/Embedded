2b5b674af59803f5b55bd6df78eef21b1e0acff2
swarnika12 <59121499+swarnika12@users.noreply.github.com>
2020-07-23 12:13:15 +0530

    Update booth_mult.v

--- booth_mult.v
+++ booth_mult.v
@@ -1,12 +1,4 @@
-////////////////////////////////////////////////////////////////////////////////
-//                        Coypright (C) Aviral Mittal.
-////////////////////////////////////////////////////////////////////////////////
-//   All rights reserved. Reproducion in whole or in part is prohibited without
-//   written consent of copyright Owner.The professional use of this material
-//   is subject to the copy right owners approval in written.
-////////////////////////////////////////////////////////////////////////////////
-//   Comments welcome on aviral.mittal@sli-institute.ac.uk or avimit@yahoo.com
-////////////////////////////////////////////////////////////////////////////////
+
 // It is a radix-4 booth's multiplier. It will work ONLY if width is even Number
 `define width	16
 `timescale 1ns/1ps
@@ -20,7 +12,7 @@ input	[width-1:0]		x, y;
 
 output	[width+width-1:0]	p;
 
-  reg [2:0] cc[N-1:0];
+  reg [2:0] c[N-1:0];
   reg [width:0] pp[N-1:0];
   reg [width+width-1:0] spp[N-1:0];
   reg [width+width-1:0] prod;
@@ -30,12 +22,12 @@ output	[width+width-1:0]	p;
 assign inv_x = {~x[width-1],~x}+1;
 always @ (x or y or inv_x)
 begin
-  cc[0] = {y[1],y[0],1'b0};
+  c[0] = {y[1],y[0],1'b0};
   for(kk=1;kk<N;kk=kk+1)
-    cc[kk] = {y[2*kk+1],y[2*kk],y[2*kk-1]};
+    c[kk] = {y[2*kk+1],y[2*kk],y[2*kk-1]};
   for(kk=0;kk<N;kk=kk+1)
   begin
-    case(cc[kk]) 
+    case(c[kk]) 
       3'b001 , 3'b010 : pp[kk] = {x[width-1],x};
       3'b011 : pp[kk] = {x,1'b0};
       3'b100 : pp[kk] = {inv_x[width-1:0],1'b0};
